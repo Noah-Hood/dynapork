33902ab239e3a23b071b12c01eace58430c0ed3f
Noah Hood <hood.noah@icloud.com>
2023-02-07 09:37:08 -0500

    checkpoint

--- util/src/IPAddress.Types.fs
+++ util/src/IPAddress.Types.fs
@@ -1,29 +1,35 @@
 namespace Domain
 
+// General IP Address type
 type IPAddressData =
     { Address: string
       UpdatedAt: System.DateTime }
 
 type IPAddress = IPAddress of IPAddressData
 
-type IPValidationError =
-    | Empty
-    | Malformed
+// Command(s)
+type UpdateIPCmd = Command<IPAddress>
 
-type IPCommand = UpdateIP of IPAddress
-
-type IPAddressUpdated =
+// Events
+type IPAddressEvent =
     | Unchanged of IPAddress
     | Revalidated of IPAddress
     | Changed of IPAddress
 
-type UpdateIP = IPCommand -> IPAddress -> IPAddressUpdated
+// Errors
+type IPValidationError =
+    | Empty
+    | Malformed
+
+// UpdateIP workflow
+type UpdateIP = UpdateIPCmd -> IPAddress -> IPAddressEvent
 
 module IPAddress =
     open System.Text.RegularExpressions
 
     [<Literal>]
-    let private regexPattern = @"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$"
+    let private regexPattern =
+        @"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$"
 
     /// <summary>Constructor for IPAddress type</summary>
     let create (str: string) =
