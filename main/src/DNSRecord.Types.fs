eed9899b979db6bb94c3546611d91149dea0b22d
Noah Hood <hood.noah@icloud.com>
2023-02-08 13:28:59 -0500

    begin a simplified version, starting with JSON processing

---
+++ main/src/DNSRecord.Types.fs
@@ -0,0 +1,65 @@
+namespace Domain
+
+open Thoth.Json
+
+module DNSRecord =
+    type PBPingCommand =
+        { SecretAPIKey: string
+          APIKey: string }
+
+    type PBPingSuccessResponse = { Status: string; YourIp: string }
+
+    type PBPingFailureResponse = { Status: string; Message: string }
+
+    type PBPingResponse =
+        | PBPingSuccess of PBPingSuccessResponse
+        | PBPingFailure of PBPingFailureResponse
+
+
+    // Thoth coders
+    module PBPingCommand =
+        let encoder (cmd: PBPingCommand) =
+            Encode.object [ "secretapikey", Encode.string cmd.SecretAPIKey
+                            "apikey", Encode.string cmd.APIKey ]
+
+        let decoder: Decoder<PBPingCommand> =
+            Decode.object (fun get ->
+                { PBPingCommand.SecretAPIKey = get.Required.Field "secretapikey" Decode.string
+                  PBPingCommand.APIKey = get.Required.Field "apikey" Decode.string })
+
+    module PBPingSuccessResponse =
+        let encoder (successResponse: PBPingSuccessResponse) =
+            Encode.object [ "status", Encode.string successResponse.Status
+                            "yourIp", Encode.string successResponse.YourIp ]
+
+        let decoder: Decoder<PBPingSuccessResponse> =
+            Decode.object (fun get ->
+                { PBPingSuccessResponse.Status = get.Required.Field "status" Decode.string
+                  PBPingSuccessResponse.YourIp = get.Required.Field "yourIp" Decode.string })
+
+    module PBPingFailureResponse =
+        let encoder (failureResponse: PBPingFailureResponse) =
+            Encode.object [ "status", Encode.string failureResponse.Status
+                            "message", Encode.string failureResponse.Message ]
+
+        let decoder: Decoder<PBPingFailureResponse> =
+            Decode.object (fun get ->
+                { PBPingFailureResponse.Status = get.Required.Field "status" Decode.string
+                  PBPingFailureResponse.Message = get.Required.Field "message" Decode.string })
+
+    module PBPingResponse =
+
+        let encoder (resp: PBPingResponse) =
+            match resp with
+            | PBPingSuccess s ->
+                Encode.object [ "status", Encode.string s.Status
+                                "yourIp", Encode.string s.YourIp ]
+            | PBPingFailure f ->
+                Encode.object [ "status", Encode.string f.Status
+                                "message", Encode.string f.Message ]
+
+        let decoder: Decoder<PBPingResponse> =
+            Decode.oneOf [ PBPingSuccessResponse.decoder
+                           |> Decode.map PBPingSuccess
+                           PBPingFailureResponse.decoder
+                           |> Decode.map PBPingFailure ]
---
+++ main/src/Program.fs
@@ -0,0 +1,41 @@
+﻿open System.Net.Http
+
+// step 1: find public IP
+let findPublicIP (withClient: HttpClient) =
+    let ipUrl = "https://icanhazip.com"
+
+    async {
+        let! pubIp =
+            withClient.GetStringAsync(ipUrl)
+            |> Async.AwaitTask
+
+        return pubIp
+    }
+
+// step 2: create a timer which checks for a new IP Address every thirty seconds
+let createObservableTimer interval =
+    let timer = new System.Timers.Timer(float interval)
+    timer.AutoReset <- true // makes timer repeat itself after elapsing
+
+    let observable = timer.Elapsed
+
+    let task =
+        async {
+            timer.Start()
+            do! Async.Sleep 120000 // run for a minute and a half; removing this will make it run indefinitely
+            timer.Stop() // stop timer
+        }
+
+    (task, observable)
+
+[<EntryPoint>]
+let main _ =
+    use httpClient = new HttpClient()
+
+    let (timer, eventStream) = createObservableTimer 1000
+
+    eventStream
+    |> Observable.subscribe (fun _ -> printfn "tick %A" System.DateTime.Now)
+    |> ignore
+
+    0
