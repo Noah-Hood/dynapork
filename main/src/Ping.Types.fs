177e40ddde275a04499d8916a6bec08e7f91e5cd
Noah Hood <hood.noah@icloud.com>
2023-02-13 14:08:36 -0500

    streamline the ping types/functions

--- main/src/Ping.Types.fs
+++ main/src/Ping.Types.fs
@@ -3,6 +3,7 @@ namespace Domain
 open System.Net.Http
 
 open Thoth.Json.Net
+open PorkBunError
 
 module Ping =
     type PBPingCommand =
@@ -10,19 +11,14 @@ module Ping =
           APIKey: string }
 
     type PBPingSuccessResponse = { Status: string; YourIP: string }
-
     type PBPingFailureResponse = { Status: string; Message: string }
 
     type IPAddress = IPAddress of string
 
-    type PBPingResponse =
-        | PBPingSuccess of PBPingSuccessResponse
-        | PBPingFailure of PBPingFailureResponse
-
     type PBPingError =
         | InvalidAPIKey
         | JSONDecodeFailure of string
-        | Unknown
+        | APIError of string
 
     type PBPingResult = Result<IPAddress, PBPingError>
 
@@ -50,27 +46,5 @@ module Ping =
                   PBPingSuccessResponse.YourIP = get.Required.Field "yourIp" Decode.string })
 
     module PBPingFailureResponse =
-        let encoder (failureResponse: PBPingFailureResponse) =
-            Encode.object [ "status", Encode.string failureResponse.Status
-                            "message", Encode.string failureResponse.Message ]
-
-        let decoder: Decoder<PBPingFailureResponse> =
-            Decode.object (fun get ->
-                { PBPingFailureResponse.Status = get.Required.Field "status" Decode.string
-                  PBPingFailureResponse.Message = get.Required.Field "message" Decode.string })
-
-    module PBPingResponse =
-        let encoder (resp: PBPingResponse) =
-            match resp with
-            | PBPingSuccess s ->
-                Encode.object [ "status", Encode.string s.Status
-                                "yourIp", Encode.string s.YourIP ]
-            | PBPingFailure f ->
-                Encode.object [ "status", Encode.string f.Status
-                                "message", Encode.string f.Message ]
-
-        let decoder: Decoder<PBPingResponse> =
-            Decode.oneOf [ PBPingSuccessResponse.decoder
-                           |> Decode.map PBPingSuccess
-                           PBPingFailureResponse.decoder
-                           |> Decode.map PBPingFailure ]
+        let encoder = PBErrorResponse.encoder
+        let decoder = PBErrorResponse.decoder
--- main/src/Program.fs
+++ main/src/Program.fs
@@ -3,34 +3,43 @@
 open Functions.EditDNSRecord
 open Domain.EditDNSRecord
 
-
 [<EntryPoint>]
 let main _ =
     use client = new HttpClient()
     let secretKey = Secrets.PBAPISecretKey
     let apiKey = Secrets.PBAPIKey
 
-    let bodyParams =
-        { SecretAPIKey = secretKey
-          APIKey = apiKey
-          Name = None
-          Type = "A"
-          Content = "8.8.8.8"
-          TTL = None
-          Prio = None }
+    let pingCmd: Domain.Ping.PBPingCommand =
+        { APIKey = apiKey
+          SecretAPIKey = secretKey }
 
-    let urlParams =
-        { Domain = "noah-hood.io"
-          Subdomain = None }
+    // let bodyParams =
+    //     { SecretAPIKey = secretKey
+    //       APIKey = apiKey
+    //       Name = None
+    //       Type = "A"
+    //       Content = "8.8.8.8"
+    //       TTL = None
+    //       Prio = None }
 
-    let cmd =
-        { BodyParams = bodyParams
-          URLParams = urlParams }
+    // let urlParams =
+    //     { Domain = "noah-hood.io"
+    //       Subdomain = None }
+
+    // let cmd =
+    //     { BodyParams = bodyParams
+    //       URLParams = urlParams }
+
+    // async {
+    //     let! result = editRecord client cmd
+
+    //     printfn "%A" result
+    // }
+    // |> Async.RunSynchronously
 
     async {
-        let! result = editRecord client cmd
-
-        printfn "%A" result
+        let! pingResponse = Functions.Ping.fetchIP client pingCmd
+        printfn "%A" pingResponse
     }
     |> Async.RunSynchronously
 
