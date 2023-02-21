7d66b6ed528ca2eb5e6c391ab8ff15c5686dd65b
Noah Hood <hood.noah@icloud.com>
2023-02-21 13:08:24 -0500

    add test cases for DEV case of loadProgramEnvironment

--- main/test/Environment.Functions.Test.fs
+++ main/test/Environment.Functions.Test.fs
@@ -8,8 +8,41 @@ open Domain.Environment
 let loadProgramEnvironmentTests =
     testList
         "loadProgramEnvironment test scenarios"
-        [ testCase "returns 'dev' for development"
+        [ testCase "returns DEV for 'development'"
           <| fun _ ->
               let result = loadProgramEnvironment "development"
 
-              Expect.equal result DEV "did not return DEV when provided 'development'" ]
+              Expect.equal result DEV "did not return DEV when provided 'development'"
+
+          testCase "returns DEV for 'develop'"
+          <| fun _ ->
+              let result = loadProgramEnvironment "develop"
+
+              Expect.equal result DEV "did not return DEV when provided 'develop'"
+
+          testCase "returns DEV for 'dev'"
+          <| fun _ ->
+              let result = loadProgramEnvironment "dev"
+
+              Expect.equal result DEV "did not return DEV when provided 'dev'"
+
+          testCase "returns DEV for 'DEVELOPMENT'"
+          <| fun _ ->
+              let result = loadProgramEnvironment "DEVELOPMENT"
+
+              Expect.equal result DEV "did not return DEV when provided 'DEVELOPMENT'"
+
+          testCase "returns DEV for 'DEVELOP'"
+          <| fun _ ->
+              let result = loadProgramEnvironment "DEVELOP"
+
+              Expect.equal result DEV "did not return DEV when provided 'DEVELOP'"
+
+          testCase "returns DEV for 'DEV'"
+          <| fun _ ->
+              let result = loadProgramEnvironment "DEV"
+
+              Expect.equal result DEV "did not return DEV when provided 'DEV'"
+
+
+          ]
