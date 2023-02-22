355fbfd707d48f075cab77158ac4c132a991fe7b
Noah Hood <86262760+hoodnoah@users.noreply.github.com>
2023-02-22 13:11:11 -0500

    Implement unit tests for Environment.Functions.fs (#10)
    * implement working testcase for Environment
    
    * add test cases for DEV case of loadProgramEnvironment
    
    * implement basic unit tests for loadProgramEnvironment
    
    * implement basic property-based tests for loadProgramEnvironment

--- main/src/Environment.Functions.fs
+++ main/src/Environment.Functions.fs
@@ -6,23 +6,30 @@ open System
 open Domain.Environment
 
 module Environment =
-    let loadProgramEnvironment:LoadProgramEnvironment = 
+    let loadProgramEnvironment: LoadProgramEnvironment =
         fun s ->
-            match s.ToLowerInvariant() with
-            | "development" | "dev" | "develop" -> DEV
-            | "production" | "prod" -> PROD
-            | _ -> failwith "Environment environment variable must be set to one of 'dev' or 'prod.'"
-        
-    let private addEnvVars env (cfgBldr: IConfigurationBuilder) = 
-        cfgBldr.AddEnvironmentVariables()
+            try
+                match s.ToLowerInvariant() with
+                | "development"
+                | "dev"
+                | "develop" -> DEV
+                | "production"
+                | "prod" -> PROD
+                | _ -> failwith "Environment environment variable must be set to one of 'dev' or 'prod.'"
+            with
+            | :? NullReferenceException ->
+                failwith "Environment environment variable must be set to one of 'dev' or 'prod.'"
 
-    let private addRuntimeSecrets env (cfgBldr: IConfigurationBuilder) = 
+    let private addEnvVars env (cfgBldr: IConfigurationBuilder) = cfgBldr.AddEnvironmentVariables()
+
+    let private addRuntimeSecrets env (cfgBldr: IConfigurationBuilder) =
         match env with
         | DEV -> cfgBldr.AddUserSecrets<EnvironmentVariables>()
         | PROD -> cfgBldr.AddKeyPerFile("/var/secrets", true)
 
-    let private tryGetItemFromConfig (config: IConfigurationRoot) (itemName: string) = 
+    let private tryGetItemFromConfig (config: IConfigurationRoot) (itemName: string) =
         let rawKey = config.Item(itemName)
+
         match String.IsNullOrEmpty(rawKey) with
         | true -> None
         | false -> Some rawKey
@@ -30,47 +37,52 @@ module Environment =
 
     let loadEnvironment: LoadEnvironment =
         fun env ->
-            let config = (new ConfigurationBuilder()) |> addEnvVars env |> addRuntimeSecrets env |> (fun x -> x.Build())
+            let config =
+                (new ConfigurationBuilder())
+                |> addEnvVars env
+                |> addRuntimeSecrets env
+                |> (fun x -> x.Build())
 
             let tryGetPartial = tryGetItemFromConfig config
 
-            let apiKey = 
+            let apiKey =
                 match tryGetPartial "APIKEY" with
                 | Some a -> a |> APIKey
                 | None -> failwith "APIKEY environment variable must be set."
 
-            let secretKey = 
+            let secretKey =
                 match tryGetPartial "SECRETKEY" with
                 | Some s -> s |> SecretKey
                 | None -> failwith "SECRETKEY environment variable must be set."
 
-            let domainName = 
+            let domainName =
                 match tryGetPartial "DOMAINNAME" with
                 | Some d -> d |> DomainName
                 | None -> failwith "DOMAINNAME environment variable must be set."
 
-            let recordType = 
+            let recordType =
                 match tryGetPartial "RECORDTYPE" with
-                | Some r -> 
+                | Some r ->
                     match r |> RecordType.stringToRecordType with
                     | Some rt -> rt
-                    | None -> failwith "RECORDTYPE environment variable set to invalid value. Must be onf of A, MX, CNAME, ALIAS, TXT, Ns, AAAA, ETV, TLSA, or CAA."
+                    | None ->
+                        failwith
+                            "RECORDTYPE environment variable set to invalid value. Must be onf of A, MX, CNAME, ALIAS, TXT, Ns, AAAA, ETV, TLSA, or CAA."
                 | None -> failwith "RECORDTYPE environment variable must be set."
 
-            let subdomain = 
-                match tryGetPartial "SUBDOMAIN" with    
+            let subdomain =
+                match tryGetPartial "SUBDOMAIN" with
                 | Some s -> s |> Subdomain |> Some
                 | None -> None
 
             let credentials =
                 { APIKey = apiKey
-                  SecretKey = secretKey}
+                  SecretKey = secretKey }
 
-            let domainInfo = {
-                Domain = domainName
-                Subdomain = subdomain
-                RecordType = recordType
-            }
+            let domainInfo =
+                { Domain = domainName
+                  Subdomain = subdomain
+                  RecordType = recordType }
 
             { Credentials = credentials
               DomainInfo = domainInfo }
