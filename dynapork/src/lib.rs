f3156aaa24658df85ad7fc59fb898b5796d88693
Noah Hood <hood.noah@icloud.com>
2023-04-01 17:05:18 -0400

    factor config out into lib.rs, begin implementing ip_fetching w/ reqwest

--- dynapork/src/lib.rs
+++ dynapork/src/lib.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "docker")]
 pub mod docker_secrets {
     use std::fs::File;
     use std::io::Read;
@@ -20,12 +21,89 @@ pub mod docker_secrets {
     }
 
     /// Get a secret from the docker secrets directory by name
-    pub fn get_secret(secret_name: &str) -> Result<String, SecretError> {
+    pub fn try_get_secret_by_name(secret_name: &str) -> Result<String, SecretError> {
         let secret_file = File::open(format!("{SECRET_PATH}/{secret_name}"));
 
         match secret_file {
             Ok(mut file) => try_get_string_from_file(&mut file),
             Err(_) => Err(SecretError::FileNotFound),
         }
+    }
+}
+
+pub mod config {
+    use std::env;
+
+    #[derive(Debug)]
+    pub struct Credentials {
+        pub api_key: String,
+        pub api_secret: String,
+    }
+
+    #[derive(Debug)]
+    pub struct Config {
+        pub credentials: Credentials,
+        pub domain: String,
+        pub subdomain: Option<String>,
+    }
+
+    #[derive(Debug)]
+    pub enum ConfigError {
+        ApiKeyNotFound,
+        ApiSecretNotFound,
+        DomainNotFound,
+    }
+
+    #[cfg(feature = "dev")]
+    pub fn read_config() -> Result<Config, ConfigError> {
+        // load dotenv
+        dotenv::dotenv().ok();
+
+        // get api key
+        let api_key = env::var("API_KEY").map_err(|_| ConfigError::ApiKeyNotFound)?;
+
+        // get api secret
+        let api_secret = env::var("API_SECRET").map_err(|_| ConfigError::ApiSecretNotFound)?;
+
+        let credentials = Credentials {
+            api_key,
+            api_secret,
+        };
+
+        // get domain
+        let domain = env::var("DOMAIN").map_err(|_| ConfigError::DomainNotFound)?;
+
+        // get subdomain
+        let subdomain = env::var("SUBDOMAIN").ok();
+
+        Ok(Config {
+            credentials,
+            domain,
+            subdomain,
+        })
+    }
+
+    #[cfg(feature = "docker")]
+    pub fn read_config() -> Result<Config, ConfigError> {
+        // get api key
+        let api_key = docker_secrets::try_get_secret_by_name("API_KEY")
+            .map_err(|_| ConfigError::ApiKeyNotFound)?;
+
+        // get api secret
+        let api_secret = docker_secrets::try_get_secret_by_name("API_SECRET")
+            .map_err(|_| ConfigError::ApiSecretNotFound)?;
+
+        // get domain
+        let domain = env::var("DOMAIN").map_err(|_| ConfigError::DomainNotFound)?;
+
+        // get subdomain
+        let subdomain = env::var("SUBDOMAIN").ok();
+
+        Ok(Config {
+            api_key,
+            api_secret,
+            domain,
+            subdomain,
+        })
     }
 }
--- dynapork/src/main.rs
+++ dynapork/src/main.rs
@@ -1,3 +1,28 @@
+use dynapork::config;
+
+pub mod request_ip {
+    use super::config;
+    use reqwest;
+    pub fn request_ip(credentials: config::Credentials) -> Result<String, reqwest::Error> {
+        let client = reqwest::blocking::Client::new();
+        let url = "https://api.ipify.org";
+        let response = client.get(url).send()?;
+        let ip = response.text()?;
+        Ok(ip)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // step 1: load configuration
+    let config = match config::read_config() {
+        Ok(config) => config,
+        Err(err) => {
+            eprintln!("Error loading configuration: {:?}", err);
+            std::process::exit(1)
+        }
+    };
+
+    println!("{:?}", config);
+
+    // step 2: request ip address
 }
