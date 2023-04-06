89102d877d3ccb7e2bb0a291493f02b4ee0803aa
Noah Hood <hood.noah@icloud.com>
2023-04-06 15:53:25 -0400

    failing test for ping when failure response rec'd

--- src/ping.rs
+++ src/ping.rs
@@ -7,8 +7,39 @@ use crate::constants::PING_URL;
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct PingSuccessResponse {
     status: String,
+    #[serde(rename = "yourIP")]
     your_ip: String,
 }
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct PingFailureResponse {
+    status: String,
+    message: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(untagged)]
+pub enum PingResponse {
+    Success(PingSuccessResponse),
+    Failure(PingFailureResponse),
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+pub enum PingError {
+    InvalidCredentials,
+    Unknown,
+}
+
+impl std::fmt::Display for PingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PingError::InvalidCredentials => write!(f, "Invalid credentials"),
+            PingError::Unknown => write!(f, "Unknown error"),
+        }
+    }
+}
+
+impl Error for PingError {}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Credentials {
@@ -16,14 +47,23 @@ pub struct Credentials {
     secret_key: String,
 }
 
+/// Pings the Porkbun API with the provided client, returning the IP Address
+/// of the caller, if successful, and an error otherwise.
 pub fn ping(
     client: &impl HttpClient<ResponseBody = String>,
     credentials: &Credentials,
 ) -> Result<String, Box<dyn Error>> {
     let response_body = client.post_json(PING_URL, credentials)?;
 
-    let response: PingSuccessResponse = serde_json::from_str(&response_body)?;
-    Ok(response.your_ip)
+    let response = serde_json::from_str::<PingResponse>(&response_body)?;
+
+    match response {
+        PingResponse::Success(success) => Ok(success.your_ip),
+        PingResponse::Failure(failure) => match failure.message.as_str() {
+            "Invalid API key. (002)" => Err(Box::new(PingError::InvalidCredentials)),
+            _ => Err(Box::new(PingError::Unknown)),
+        },
+    }
 }
 
 #[cfg(test)]
@@ -79,5 +119,23 @@ mod ping_tests {
         let result = super::ping(&client, &default_credentials).unwrap();
 
         assert_eq!(result, IP_ADDRESS);
+    }
+
+    #[test]
+    fn returns_invalid_credentials_error_on_correct_failure() {
+        let default_credentials = Credentials {
+            api_key: "AK".to_string(),
+            secret_key: "SK".to_string(),
+        };
+        let client = MockHttpClient {
+            response_body: String::from(r#"{"status":"ERROR","message":"Invalid API key. (002)"}"#),
+        };
+
+        let result = ping(&client, &default_credentials).unwrap_err();
+
+        assert_eq!(
+            result.downcast_ref::<PingError>(),
+            Some(&PingError::InvalidCredentials)
+        );
     }
 }
